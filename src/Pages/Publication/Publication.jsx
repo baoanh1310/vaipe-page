9e9202cde6a5ed5a8ecd7a543c560b1c211a9de9
baoanh1310 <dataholicguy@gmail.com>
2022-04-06 16:40:30 +0700

    add publication page

--- src/Pages/Publication/Publication.jsx
+++ src/Pages/Publication/Publication.jsx
@@ -7,61 +7,42 @@ const Publications = () => {
       <div 
         className="block_title"
         style={{
-          backgroundImage: `url("${require("../../images/header/publication.png")}"`
+          backgroundImage: `url("${require("../../images/header/publications.png")}"`
         }}
       >Publications</div>
       {/* <div className="tba">TO BE ANNOUNCED</div> */}
       <div className="publications">
         <div className="publications_journals">
           <div className="title">Journal papers</div>
-          <p>[1] Hong Son Vu, Ngan Nguyen, Nam Ha-Van, Chulhun Seo, and Minh Thuy Le, 
-            “Multiband Ambient RF Energy Harvesting for Autonomous IoT Devices", 
-            IEEE MICROWAVE AND WIRELESS COMPONENTS LETTERS, 2020.</p>
-          <p>[2] Kien Nguyen, Phi Le Nguyen, Zhetao Li, Hiroo Sekiya, 
-            “Empowering 5G Mobile Devices with Network Softwarization”, 
-            IEEE Transactions on Network and Service Management, 
-            Vol. 18, Issue 3, pp. 2492-2501, September 2021.</p>
-          <p>[3] Minh Hieu Nguyen, Phi Le Nguyen, Kien Nguyen, Van An Le, Thanh-Hung Nguyen, Yusheng Ji, 
-            “PM2.5 Prediction Using Genetic Algorithm-based Feature Selection and Encoder-Decoder Model”,
-             IEEE Access, Vol. 9, pp. 57338 - 57350, 2021.</p>
-          <p>[4] Chen Xuan, Kien Nguyen, Hiroo Sekiya,
-             “An Experimental Study on Performance of Private Blockchain in IoT Applications”, 
-             Elsevier Peer-to-Peer Networking and Applications,
-              Vol. 14, issue 5, pp. 3075–3091, September 2021.</p>
-          <p>[5] Nguyen, Cam Ly, Orestis Georgiou, Gabriele Gradoni, and Marco Di Renzo. 
-            "Wireless Fingerprinting Localization in Smart Environments using Reconfigurable Intelligent Surfaces." 
-            IEEE Access (2021).</p>
-          <p>[6] Minh Q. Dinh, Minh Thuy Le, 
-            "Triplexer-Based Multiband Rectenna for Ambient RF Energy Harvesting from 3G/4G and Wi-Fi",
-             IEEE Microwave and Wireless Components Letters, Vol. 31, No.9, 1094-1097</p>
-          <p>[7] Minh Dinh, Nam Ha-Van, Nguyen Thanh Tung, Minh Thuy Le, 
-            “Dual-Polarized Wide-Angle Energy Harvester for Self-Powered IoT Devices”, 
-            IEEE Access 9, 103376-103384</p>
+          <p>- Haesik kim, Gabriel Porto Villardi and Jing Ma, “Energy Efficient Dynamic Resource Allocation for Wireless Systems Using Receiver Puncturing Technique,” Journal of Communications and Networks, Vol. 20, no.4, August 2018.</p>
+          <p>- M. López-Benítez, A. Al-Tahmeesschi, D. K. Patel, J. Lehtomäki and K. Umebayashi, "Estimation of Primary Channel Activity Statistics in Cognitive Radio Based on Periodic Spectrum Sensing Observations," in IEEE Transactions on Wireless Communications, vol. 18, no. 2, pp. 983-996, Feb. 2019.</p>
+          <p>- Razvan-Andrei Stoica, Giuseppe Thadeu Freitas De Abreu, Takanori Hara, and Koji Ishibashi, "Massively Concurrent Non-Orthogonal Multiple Access for 5G Networks and Beyond," IEEE Access, Volume: 7, pp82080 - 82100, 19 June 2019.</p>
         </div>
         <div className="publications_conferences">
-          <div className="title">Conference papers</div>
-          <p>[1] Viet Dung Nguyen, Phi Le Nguyen, Trung Hieu Nguyen, Kien Nguyen, 
-            Phan Thuan Do, “An (e-1)/(2e-1)-Approximation Algorithm for Maximizing the Coverage 
-            Capability in Vehicle-based Mobile Air Quality Monitoring Systems”, 
-            The 19th IEEE International Symposium on Network Computing and Applications (NCA 2020).</p>
-          <p>[2] La Van Quan, Phi Le Nguyen, Thanh-Hung Nguyen, Kien Nguyen, “Q-learning-based,
-             Optimized On-demand Charging Algorithm in WRSN”, The 19th IEEE International 
-             Symposium on Network Computing and Applications (NCA 2020).</p>
-          <p>[3] Nguyen Thi Hanh, Huynh Thi Thanh Binh and Nguyen Van Son, "Minimal Relay Node Placement for 
-             Ensuring Network Connectivity in Mobile Wireless Sensor Networks", The 19th IEEE International 
-             Symposium on Network Computing and Applications (NCA 2020).</p>
-          <p>[4] Viet-Dung Nguyen, Phi Le Nguyen, Trung Hieu Nguyen, Phan Thuan Do, “A 1/2-Approximation 
-             Algorithm for Target Coverage Problem in Mobile Air Quality Monitoring Systems”, The 2020 IEEE 
-             Global Communications Conference (IEEE GLOBECOM), 8–10 December 2020, Taipei, Taiwan.</p>
-          <p>[5] Do Bao Son, Vu Tri An, Trinh Thu Hai, Binh Minh Nguyen, Phi Le Nguyen, Huynh Thi Thanh Binh, 
-             “Fuzzy Deep Q-learning Task Offloading in Delay Constrained Vehicular Fog Computing”,
-             International Joint Conference on Neural Network, IJCNN 2021 (Accepted)</p>
-          <p>[6] Viet An Nguyen, Viet Hung Vu, Van Sang Doan, Thanh Hung Nguyen, Do Phan Thuan, Kien Nguyen, Phi Le Nguyen, Minh Thuy Le, 
-            “Realizing Mobile Air Quality Monitoring System: Architectural Concept and Device Prototype”, 
-            IEEE APCC 2021, pp. , October 2021. </p>
-          <p>[7] Ryota Nakada, Zhetao Li, Tingrui Pei, Kien Nguyen, Hiroo Sekiya, 
-            “An IOTA-Based Micropayment System for Air Quality Monitoring Application”, 
-            IEEE VTC Fall 2021, pp. 1-6, September 2021</p>
+          <div className="title">Conference and workshop papers</div>
+          <p>- K. Umebayashi, Y. Kimoto, A. Tölli, "Adaptive Antenna Array with weight and antenna space control", IEEE WCNC 2020. - A. Al-Tahmeesschi, K. Umebayashi, H. Iwata, M. Lopez-Benitez, J. Lehtomaki, "Applying Deep Neural Networks for Duty Cycle Estimation", IEEE WCNC 2020.</p>
+          <p>- A. Al-Tahmeesschi, K. Umebayashi, H. Iwata, M. Lopez-Benitez, J. Lehtomaki, "Applying Deep Neural Networks for Duty Cycle Estimation", IEEE WCNC 2020.</p>
+          <p>- P. Shah, S. Joshi, A. Tölli, K. Umebayashi, "Direct Beamformer Estimation for Hybrid Architecture in mmWave Dynamic TDD System", IEEE WCNC 2020-Workshop.</p>
+          <p>- H. Iwata, K. Umebayashi, A. Al-Tahmeesschi, S. Joshi, M. Lopez-Benitez, J. Lehtomaki, "A Study on High-Efficiency Energy Detection-Based Spectrum Measurements", IEEE WCNC 2020-Workshop.</p>
+          <p>- A. Arvola, S. Joshi, A. Tölli, “Dynamic UL/DL Mode Selection and Resource Allocation in Multi-Cell MIMO TDD Systems”, Asilomar 2019.</p>
+          <p>- P. Shah, S. Joshi, A. Tölli, K. Umebayashi, “Direct Beamformer Estimation for Hybrid Architecture in mmWave Dynamic TDD System”, IEEE WCNC 2020-Workshop.</p>
+          <p>- P. Jayasinghe, A. Tölli, J. Kaleva, M. Latva-aho “Traffic Aware Beamformer Design for Integrated Access and Backhaul with Flexible TDD”, IEEE WCNC 2020</p>
+          <p>- Uitto, M. & Heikkinen, A. & Rantala, S.J.. & Mäkelä, J. :“ Evaluation of Evolved Multimedia Broadcast Multicast Service for More Efficient Mobile Video Streaming”, ISM'19, San Diego, CA, USA, December 2019.</p>
+          <p>- Uitto, M. & Hoppari, M. & Heikkilä, T. & Isto, P. & Anttonen, A. & Mämmelä, A. :“Remote Control Demonstrator Development in 5G Test Network”, EuCNC'19, Valencia, Spain, June 2019.</p>
+          <p>- Hiroki Iwata, Kenta Umebayashi, Ahmed Al-Tahmeesschi, Miguel Lopez-Benitez and Janne Lehtomäki "Time and Frequency Varying Noise Floor Estimation for Spectrum Usage Measurement" WCNC'19, Marrakech, Morocco, April 2019.</p>
+          <p>- Yuya Aoki, Takeo Fujii, “Spectrum database construction based on HMM and spectrum sharing in multiple primary users environment,” Proc. ICAIIC2019, Okinawa, Japan, Feb. 2019.</p>
+          <p>- Hirofumi Nakajo, Yuya Aoki, Keita Katagiri, Takeo Fuji "High Density Spectrum Sharing Method among Micro Operators considering Spectrum Database," IWSS2019, Marrakech, Morocco, April 2019.</p>
+          <p>- T. Hara, A. Stoica, K. Ishibashi, and G. Abreu, “On the Sum-Rate Capacity and Spectral Efficiency Gains of Massively Concurrent NOMA Systems,” WCNC'19, Marrakech, Morocco, Apr. 2019.</p>
+          <p>- Keita Katagiri, Keita Onose, Koya Sato, Kei Inage, Takeo Fujii, "Highly accurate estimation of radio propagation using model classifier," IEICE Tech. Rep., SR2018-108, Jan. 2019.</p>
+          <p>- Hiroki Iwata, Kenta Umebayash, Ahmed Al-Tahmeesschi, Miguel Lopez-Benitez, "Neural Network-based Channel Occupancy Rate Prediction" IEICE SmartCom SR2018-88, pp. 75-82, Oct. 2018.</p>
+          <p>- Ryo Sato, Kenta Umebayashi, K. B. Shashika Manosha, "Efficient channel allocation for Citizens broadband radio service/spectrum access system" IEICE SmartCom SR2018-89, pp. 83-84, Oct. 2018.</p>
+          <p>- Akihisa Abe, Yuya Aoki, Takeo Fujii, "Time Series Analysis of Multiple Primary Users by Cooperative Sensing based on SDAR Algorithm," The 5th International Workshop on Smart Wireless Communications, Bangkok, Thailand, Oct. 2018.</p>
+          <p>- Yuya Aoki, Takeo Fujii, "Spectrum Sharing in Cooperation with Time- Space Radio Environment Database," The 5th International Workshop on Smart Wireless Communications, Bangkok, Thailand, Oct. 2018.</p>
+          <p>- Hirofumi Nakajo, Yuya Aoki, Keita Katagiri, Takeo Fujii, "Spatial Spectrum Sharing among Micro Operators considering Distribution on Measurement-based Spectrum Database," The 5th International Workshop on Smart Wireless Communications, Bangkok, Thailand, Oct. 2018.</p>
+          <p>- Ryuji Kobayashi and Koichi Adachi, “Computational Capability and Radio Resource Allocation for Mobile Edge Computing,” The 5th International Workshop on Smart Wireless Communications, Bangkok, Thailand, Oct. 2018.</p>
+          <p>- Takanori Hara, Koji Ishibashi, Soon X. Ng, and Lajos Hanzo, “Low- Complexity Generator Polynomial Search for Turbo Trellis-Coded Spatial Modulation Using Symbol-based EXIT Charts,” in Proc. 10th International Symposium on Turbo Codes & Iterative Information Processing, Hong-Kong, Dec. 2018. (Accepted)</p>
+          <p>- Kazuya Ohira, Koji Ishibashi, "Aggregate-Compression-Aided Subcarrier IQ Index Modulation with Discreteness-Aware Fast Iterative Shrinkage- Thresholding Algorithm,” Symposium on Information Theory and Its Applications (SITA) 2018, Fukushima, Japan, Dec. 2018.</p>
+          <p>- Yuya Aoki, Takeo Fujii, "Construction of Time-Space Radio Environment Database using HMM for Cooperative Sensing," International Conference on Computing, Networking and Communications (ICNC 2019), Honolulu, USA, Feb. 2019.</p>
         </div>
       </div>
     </>
