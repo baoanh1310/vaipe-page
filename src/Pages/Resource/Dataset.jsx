94563b2c951e6d4f26b639323f65d03793b1a668
baoanh1310 <dataholicguy@gmail.com>
2022-05-03 00:30:45 +0700

    Change content of Resource section

--- src/Pages/Resource/Dataset.jsx
+++ src/Pages/Resource/Dataset.jsx
@@ -1,29 +1,50 @@
 import React from 'react';
 import './resource.sass';
 import datasets from '../../images/body/resources/datasets.png';
+import resource4 from "../../images/body/resources/resource4.svg";
+import resource5 from "../../images/body/resources/resource5.svg";
+import resource6 from "../../images/body/resources/resource6.svg";
 
 const Dataset = () => {
     return (
         <div className="content">
-            <div className="labelGroup">
-                <div className="label">Dataset Description</div>
-            </div>
             <div className="textGroup">
-                <p>In the earliest days of medicine, people needed a trip to the doctor like a hole in the head. Because that’s exactly what they got: Healers and witch doctors were downright wanton in their use of trepanning — the practice of sharpening a stone to cut away a section of skull in fully conscious patients. Trepanning was done to relieve headaches, remove fractured skull fragments, provide spirits with an easy entrance or escape, sometimes just to provide rondelles — the leftover bony disks valued as charms or talismans.</p>
                 <p>We divide the dataset into two parts: the training set of 15,000 scans and the test set of 3,000 scans. Each image in the training set was independently labeled by 3 radiologists, while the annotation of each image in the test set was even more carefully treated and obtained from the consensus of 5 radiologists. The labeling process was performed via our own web-based framework called VinDr Lab, which was built on top of a Picture Archiving and Communication System (PACS). A demonstration of this framework can be found here.</p>
-                <p>This dataset was used for the <span>VAIPE BigData Chest Xray Abnormalities Detection Competition</span> hosted on the Kaggle.com platform.</p>
-                <div className="imgContainer">
-                    <img src={datasets} className="datasetImg" />
-                    <div className="imgDesc">
-                        Examples of CXRs with radiologist’s annotations. Abnormal findings (local labels) marked by radiologists are plotted on the original images for visualization purpose. The global labels are in bold and listed at the bottom of each example. Better viewed on a computer and zoomed in for details.
+            </div>
+            <div className="colGroup">
+                <div className="resourceCol">
+                    <div className="colTitle">
+                        Pill Images
+                    </div>
+                    <div className="colImage">
+                        <img src={resource4} alt="" />
+                    </div>
+                    <div className="colDesc">
+                    In the emergency room where I work, I sometimes see patients who have either taken the wrong medication or the wrong dose of medication. It is a common problem. Medication errors can cause serious consequences. Doctors and pharmacists are diligent in making sure patients receive the correct medication. But mistakes happen. As a consumer, you need to protect yourself and ensure you have the correct medication.
                     </div>
                 </div>
-            </div>
-            <div className="labelGroup">
-                <div className="label">Dataset Statistics</div>
-            </div>
-            <div className="textGroup">
-                <p>Note: the numbers of positive labels were reported based on the majority vote of the participating radiologists. (*) The calculations were only based on the CXR scans where patient’s sex and age were known. (-) To preserve the integrity of the test set, its labels are not released to the public. The statistic of the labels on the test set is therefore not shown here.</p>
+                <div className="resourceCol">
+                    <div className="colTitle">
+                        Prescription Images
+                    </div>
+                    <div className="colImage">
+                        <img src={resource5} alt="" />
+                    </div>
+                    <div className="colDesc">
+                    When your doctor writes a prescription, she's advising you to take a medication that you can only get from a pharmacist. You can refer to the paper with the drug's name and the doctor's signature and the bottle of medicine as a prescription. Sometimes prescription is used in a non-medical way to simply mean a suggestion or recommendation, like laughter and friends — a great prescription for chasing away the blues.
+                    </div>
+                </div>
+                <div className="resourceCol">
+                    <div className="colTitle">
+                        Others
+                    </div>
+                    <div className="colImage">
+                        <img src={resource6} alt="" />
+                    </div>
+                    <div className="colDesc">
+                    The numbers of positive labels were reported based on the majority vote of the participating radiologists. (*) The calculations were only based on the CXR scans where patient’s sex and age were known. (-) To preserve the integrity of the test set, its labels are not released to the public. This dataset was used for the VAIPE BigData Chest Xray Abnormalities Detection Competition hosted on the Kaggle.com platform.
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Pages/Resource/Tool.jsx
+++ src/Pages/Resource/Tool.jsx
@@ -1,39 +1,49 @@
 import React from 'react';
 import './resource.sass';
-import tool from '../../images/body/resources/tool.png';
-import framework from '../../images/body/resources/framework.png';
-import tools from '../../images/body/resources/tools.png';
-import frameworks from '../../images/body/resources/frameworks.png';
+import resource1 from "../../images/body/resources/resource1.svg";
+import resource2 from "../../images/body/resources/resource2.svg";
+import resource3 from "../../images/body/resources/resource3.svg";
 
 const Tool = () => {
     return (
         <div className="content">
-            <div className="labelGroup">
-                <img src={tool} className="icon" />
-                <div className="label">Tools</div>
-            </div>
             <div className="textGroup">
                 <p>In the earliest days of medicine, people needed a trip to the doctor like a hole in the head. Because that’s exactly what they got: Healers and witch doctors were downright wanton in their use of trepanning — the practice of sharpening a stone to cut away a section of skull in fully conscious patients. Trepanning was done to relieve headaches, remove fractured skull fragments, provide spirits with an easy entrance or escape, sometimes just to provide rondelles — the leftover bony disks valued as charms or talismans.</p>
-                <p>At 7,000 years old, the stone trephine is considered the earliest surgical tool, but it’s not its antiquity that makes it important; it’s how the concept has remained relevant from the Neolithic to the now. Modern neurosurgeons don’t dangle rondelles around their necks, but they do still remove sections of skull.</p>
-                <p>The procedure, now called a craniotomy, is used to relieve pressure on a swelling brain, or grant access to a stroke victim’s hemorrhaging blood vessel, among others.</p>
-                <div className="imgContainer">
-                    <img src={tools} className="contentImg" />
-                </div>
-                <p>Although the trephine was the first tool to transform medicine, it was far from the last. Here, in no particular order, are 14 others carrying on in that heady tradition.</p>
-                <div className="subLabel">Hypodermic Needles</div>
-                <p>Physicians have wanted direct access to the bloodstream for a very long time. In 1492, as Columbus sailed the ocean blue, Pope Innocent VIII was on very a different path: He had an apoplectic stroke that year that left him in a coma. In an attempt to resuscitate him with an infusion of fresh blood, his physician cut open the veins of three healthy boys, then those of the pontiff, and stitched them all together. All four died from his macramé transfusion method.</p>
             </div>
-            <div className="labelGroup">
-                <img src={framework} className="icon" />
-                <div className="label">Frameworks</div>
-            </div>
-            <div className="textGroup">
-                <p>In the earliest days of medicine, people needed a trip to the doctor like a hole in the head. Because that’s exactly what they got: Healers and witch doctors were downright wanton in their use of trepanning — the practice of sharpening a stone to cut away a section of skull in fully conscious patients. Trepanning was done to relieve headaches, remove fractured skull fragments, provide spirits with an easy entrance or escape, sometimes just to provide rondelles — the leftover bony disks valued as charms or talismans.</p>
-                <p>But the larval marvels haven’t been consigned to the history books: Today, more than 800 U.S. health care centers have used medical maggots on patients. The immature flies are creating buzz as an important option for treating open wounds that won’t heal, caused by methicillin-resistant Staphylococcus aureus (the MRSA superbug), diabetic foot ulcers, flesh-eating disease and others.</p>
-                <div className="imgContainer">
-                    <img src={frameworks} className="contentImg" />
+            <div className="colGroup">
+                <div className="resourceCol">
+                    <div className="colTitle">
+                        Web-based Systems
+                    </div>
+                    <div className="colImage">
+                        <img src={resource1} alt="" />
+                    </div>
+                    <div className="colDesc">
+                        What is a Web Based System? A web-based system is an application that is accessed via HTTP. The term web-based is usually used to describe applications that run in a web browser. It can, though, also be used to describe applications that have a very small component of the solution loaded on the client PC.
+                    </div>
                 </div>
-                <p>Rene Laennec, a French physician, learned to tap on a patient’s chest to diagnose pulmonary and cardiac conditions from none other than Napoleon Bonaparte’s personal physician. But, like talking through a tin can telephone, it had auditory limitations. So Laennec took a more direct route: putting his ear directly on a patient’s chest to learn the difference between normal and abnormal heart and lung sounds.</p>
+                <div className="resourceCol">
+                    <div className="colTitle">
+                        API Platforms
+                    </div>
+                    <div className="colImage">
+                        <img src={resource2} alt="" />
+                    </div>
+                    <div className="colDesc">
+                        API Platform is an Open Source web framework for API-first projects. Describe the API's data model or import an existing one from Schema.org and get instantly a fully featured read/write API with REST operations, data validation, pagination, sorting, filtering, Swagger documentation, an authorization system, invalidation-based HTTP cache, nested documents, content negotiation and support for modern hypermedia formats (JSON-LD, HAL and Hydra).
+                    </div>
+                </div>
+                <div className="resourceCol">
+                    <div className="colTitle">
+                        Smartphone Applications
+                    </div>
+                    <div className="colImage">
+                        <img src={resource3} alt="" />
+                    </div>
+                    <div className="colDesc">
+                        Smartphone applications represent a technological opportunity to explore new approaches to mental health care, treatment, and research into psychiatric conditions, which in turn may better empower patients to become more actively involved in their own health care (Torous et al., 2014). Instead of relying upon retrospective data collection, smartphone applications enable a novel method of data collection.
+                    </div>
+                </div>
             </div>
         </div>
     )
