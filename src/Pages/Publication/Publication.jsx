1e88fa04320cf7ad3d4072280113d88479f9c0c0
baoanh1310 <dataholicguy@gmail.com>
2022-07-13 17:01:10 +0700

    Add new publication

--- src/Pages/Publication/Publication.jsx
+++ src/Pages/Publication/Publication.jsx
@@ -17,6 +17,7 @@ const Publications = () => {
         <div className="papers">
           <div className="paper-type">Conferences</div>
           <p>[C1] Nang Hung Nguyen, Phi Le Nguyen, Duc Long Nguyen, Trung Thanh Nguyen, Thuy Dung Nguyen, Thanh Hung Nguyen, Huy Hieu Pham, Truong Thao Nguyen. "<i>FedDRL: Deep Reinforcement Learning-based Adaptive Aggregation for Non-IID Data in Federated Learning</i>", the 51st International Conference on Parallel Processing - ICPP 2022. (Accepted).</p>
+          <p>[C2] Anh Duy Nguyen, Thuy Dung Nguyen, Huy Hieu Pham, Thanh Hung Nguyen and Phi Le Nguyen, "<i>Image-based Contextual Pill Recognition with Medical Knowledge Graph Assistance</i>", 14th Asian Conference on Intelligent Information and Database Systems (ACIIDS 2022). (Accepted)</p>
           <div className="paper-type">Journals</div>
           <p>[J1] Trung-Hieu Hoang, Mona Zehni, Huaijin Xu, George Heintz, Christopher Zallek, and Minh N. Do. "<i>Towards a Comprehensive Solution for a Vision-based Digitized Neurological Examination</i>" - IEEE Journal of Biomedical and Health Informatics (2022),  IF 5.8, DOI: 10.1109/JBHI.2022.3167927.</p>
           <div className="paper-type">Book chapters</div>
